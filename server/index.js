fb95ec6e6e65b5d791ecb83de217a1e891788e81
shibinshagit <shibinshamunna912@gmail.com>
2024-05-23 16:19:21 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const applicantRoutes = require('./routes/applicantRoutes');
-const companyRoutes = require('./routes/companyRoutes');
-const adminRoutes = require('./routes/adminRoutes');
+const applicantRoutes = require('./src/routes/applicantRoutes');
+const companyRoutes = require('./src/routes/companyRoutes');
+const adminRoutes = require('./src/routes/adminRoutes');
 
 const port = 3005;
-const host = '192.168.1.2';
+const host = '192.168.5.17';
 
 const app = express();
 app.use(cors());
